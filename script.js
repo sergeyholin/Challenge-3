df13c929f81231418152f78a9276acb255842b2d
Sergey Holin <holin_sergey@yahoo.com>
2022-06-26 23:01:44 -0700

    4th commit: 06/26/2022 @ 23:01

--- script.js
+++ script.js
@@ -14,65 +14,90 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// ----------------------------------------My Code---------------------------------------------------
 function generatePassword(){
-  // PsuedoCode
-  // PROMPT - User Input
- 
+  // PROMPT - User Input-----------------------------------------------------------------------------
   if (generateBtn) {
     var userInput =[];
   for(var i = 0; i < 1; i++) {
-    userInput.push(prompt("How many characters do you want in your password?"), confirm("Include special characters?"), confirm("Include Upper characters?"))
+    userInput.push(prompt("How many characters do you want in your password?", "8 - 128 characters"), confirm("Include special characters?"), confirm("Include upper case characters?"), confirm("Include lower case characters?"), confirm("Include numbers?"))
   }
   };
   console.log(userInput);
-  // Works, nex step------------------------------------------------------------
-  var array1 = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+  //Arrays--------------------------------------------------------------------------------------------
+  var array1 = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","+","=","|","]","}","[","{","'",";",":","/","?",".",">",",","<"]
   var array2 = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
-  var array3 = ["1","2","3","4","5","6","7","8","9","0"]
-  var array4 = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","+","=","|","]","}","[","{","'",";",":","/","?",".",">",",","<"]
-  // --------------------------------------------------------------------------------
-
-  if (userInput[0] && userInput[1] && userInput[2]) {
-    var userInputProcessed = array1.concat(array2, array3,array4);
-  } else if (!userInput[1] && !userInput[2]) {
+  var array3 = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+  var array4 = ["1","2","3","4","5","6","7","8","9","0"]
+  //Conditional statements----------------------------------------------------------------------------
+  // T T T T
+  if (userInput[0] && userInput[1] && userInput[2] && userInput[3] && userInput[4]) {
+    var userInputProcessed = array1.concat(array2, array3,array4); 
+  // T F T F
+  } else if (userInput[0] && userInput[1] && !userInput[2] && userInput[3] && !userInput[4]) {
     var userInputProcessed = array1.concat(array3);
-  } else if (!userInput[1]) {
-    var userInputProcessed = array1.concat(array2, array3);
-  } else if (!userInput[2]) {
-    var userInputProcessed = array1.concat(array3,array4);
-  };
+  } 
+  // F F T T
+  else if (userInput[0] && !userInput[1] && !userInput[2] && userInput[3] && userInput[4]) {
+    var userInputProcessed = array3.concat(array4);
+  } 
+  // F T F T
+  else if (userInput[0] && !userInput[1] && userInput[2] && !userInput[3] && userInput[4]) {
+    var userInputProcessed = array2.concat(array4);
+  } 
+  // T T F F
+  else if (userInput[0] && userInput[1] && userInput[2] && !userInput[3] && !userInput[4]) {
+    var userInputProcessed = array1.concat(array2);
+  } 
+  // T F F F
+  else if (userInput[0] && userInput[1] && !userInput[2] && !userInput[3] && !userInput[4]) {
+    var userInputProcessed = array1;
+  } 
+  // F T T T
+  else if (userInput[0] && !userInput[1] && userInput[2] && userInput[3] && userInput[4]) {
+    var userInputProcessed = array2.concat(array3, array4);
+  } 
+  // F T F F
+  else if (userInput[0] && !userInput[1] && userInput[2] && !userInput[3] && !userInput[4]) {
+    var userInputProcessed = array2;
+  } 
+  // T F T T
+  else if (userInput[0] && userInput[1] && !userInput[2] && userInput[3] && userInput[4]) {
+    var userInputProcessed = array1.concat(array3, array4);
+  } 
+  // F F T F
+  else if (userInput[0] && !userInput[1] && !userInput[2] && userInput[3] && !userInput[4]) {
+    var userInputProcessed = array3;
+  } 
+  // T T F T
+  else if (userInput[0] && userInput[1] && userInput[2] && !userInput[3] && userInput[4]) {
+    var userInputProcessed = array2.concat(array1, array4);
+  } 
+  // F F F T
+  else if (userInput[0] && !userInput[1] && !userInput[2] && !userInput[3] && userInput[4]) {
+    var userInputProcessed = array4;
+  } 
+  else if (userInput[0] && !userInput[1] && userInput[2] && userInput[3] && userInput[4]) {
+    var userInputProcessed = array2.concat(array3, array4);
+  } 
+  // T T T F
+  else if (userInput[0] && userInput[1] && userInput[2] && userInput[3] && !userInput[4]) {
+    var userInputProcessed = array2.concat(array3, array1);
+  } 
+  // F F F F
+  else if (userInput[0] && !userInput[1] && !userInput[2] && !userInput[3] && !userInput[4]) {
+    var userInputProcessed = 0;
+  } 
   console.log(userInputProcessed)
-  // -----------------------------------------------------------------------------------
-  var password =[];
+  //Randomizing input results ------------------------------------------------------------------------
+  var results =[];
   for(var i = 0; i < userInput[0]; i++) {
-    password.push(userInputProcessed[Math.floor(Math.random() * userInputProcessed.length)]);
+    results.push(userInputProcessed[Math.floor(Math.random() * userInputProcessed.length)]);
   };
+  // Turning comma seperated characters into one word blob--------------------------------------------
+  var password = results.join("")
   console.log(password)
-  // ---------------------------------------------------------
- 
-  
-  // prompt = length
-    // let pwdLength = prompt("how many characters")
-    // console.log(pwdLength)
-  // confirm - lowercase, uppercase, numeric, special characters
-    // let numbers  =  confirm("DO you want numbers");
-      // console.log(numbers);
-  // create var to hold anser to prompt/confirm 
-  // function to validate user answrs
-  // if and equalaity operators 
-   // if(numbers){
 
-  //  } 
-
-  // List of numbers, special chars, uppercase and lowercase
-    // array
-    // combine all arrays in 1 array
-    // Randomly choose characters
-    // create a string set equal to password var
-    // array method to check if something is in array
-  // generate random password
-  // return password
-  
   return password;
 }
 
